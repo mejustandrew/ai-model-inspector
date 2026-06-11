abbb5919438471d6ae8649bc3bc4252ab2b05516
mejustandrew <mihaivma4c@gmail.com>
2026-06-11 07:25:02 +0300

    reduce deployment size.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,27 @@
+import { minify } from 'html-minifier-terser';
+
 export default {
   base: './',
+  plugins: [
+    {
+      name: 'minify-html',
+      apply: 'build',
+      async transformIndexHtml(html) {
+        return minify(html, {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          decodeEntities: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          useShortDoctype: true,
+        });
+      },
+    },
+  ],
+  build: {
+    target: 'esnext',
+  },
 };
